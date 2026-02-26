1622309c976b82ae1189c3ef3f66e3e68d99716b
Phani <myworkspaceforme@gmail.com>
2026-02-26 17:41:07 +0530

    Add SQL query for combining Person and Address tables

---
+++ Easy/175. Combine Two Tables.sql
@@ -0,0 +1,45 @@
+-- Table: Person
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | personId    | int     |
+-- | lastName    | varchar |
+-- | firstName   | varchar |
+-- +-------------+---------+
+-- personId is the PRIMARY KEY (unique values).
+-- This table stores basic information about people.
+
+-- ------------------------------------------------------------
+
+-- Table: Address
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | addressId   | int     |
+-- | personId    | int     |
+-- | city        | varchar |
+-- | state       | varchar |
+-- +-------------+---------+
+-- addressId is the PRIMARY KEY (unique values).
+-- Each row contains city and state information
+-- for a person identified by personId.
+
+-- ------------------------------------------------------------
+
+-- Problem Statement:
+-- Write a SQL query to report:
+-- firstName, lastName, city, state
+-- for EACH person in the Person table.
+--
+-- If a person does NOT have an address entry
+-- in the Address table,
+-- return NULL for city and state.
+--
+-- The result can be returned in ANY order.
+
+-- ------------------------------------------------------------
+
+select p.firstName, p.lastName, a.city, a.state
+from Person as p 
+left join Address as a 
+on a.personId = p.personId;

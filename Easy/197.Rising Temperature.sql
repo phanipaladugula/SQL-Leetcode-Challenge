f40c2e43b56b2ff435061021060c41f3f9d37239
Phani <phanipaladugulaa@gmail.com>
2026-03-05 14:18:18 +0530

    Add SQL query to find rising temparature

---
+++ Easy/197.Rising Temperature.sql
@@ -0,0 +1,50 @@
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+Example:
+
+Input:
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+
+Output:
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+
+Explanation:
+On 2015-01-02, temperature increased from 10 to 25.
+On 2015-01-04, temperature increased from 20 to 30.
+*/
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;

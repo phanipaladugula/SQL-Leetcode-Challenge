396a043b8ac48b174d9dd4bb00f9e2bbbba5ae58
Phani <phanipaladugulaa@gmail.com>
2026-03-06 22:01:21 +0530

    Add SQL query to find biggest single number

---
+++ Easy/619.Biggest Single Number.sql
@@ -0,0 +1,16 @@
+-- Table
+-- MyNumbers
+-- +-----+
+-- | num |
+-- +-----+
+
+-- A single number is a number that appears exactly once.
+-- Find the largest single number.
+-- If no such number exists return NULL.
+
+select max(num) as num from 
+(
+select m.num from Mynumbers m
+group by m.num 
+having count(*) = 1
+) as value
